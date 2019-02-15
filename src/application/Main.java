4587a186c28149f92ef44ccf16cc3239e0fc6da8
nevergiveup10 <smolensky.p@gmail.com>
2019-02-15 14:39:15 +0300

    Hello world program

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,32 @@
 package application;
-	
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
-
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+import javafx.scene.Group;
+import javafx.scene.text.Text;
+ 
+public class Main extends Application{
+     
+    public static void main(String[] args) {
+         
+        Application.launch(args);
+    }
+     
+    @Override
+    public void start(Stage stage) {
+         
+        // установка надписи
+        Text text = new Text("Hello World!");
+        text.setLayoutY(80);    // установка положения надписи по оси Y
+        text.setLayoutX(100);   // установка положения надписи по оси X
+         
+        Group group = new Group(text);
+         
+        Scene scene = new Scene(group);
+        stage.setScene(scene);
+        stage.setTitle("First Application");
+        stage.setWidth(300);
+        stage.setHeight(250);
+        stage.show();
+    }
 }
